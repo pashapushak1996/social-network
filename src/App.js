a2c8cc174f74e8f4b0a4fd90754a43750c175dc6
pashapushak1996 <pashapushak1996@gmail.com>
2021-03-04 02:44:04 +0200

    l43

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import {Route} from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
-const App = ({state,dispatch}) => {
+const App = ({state, dispatch}) => {
     return (<div className="app-wrapper">
-            <Header/>
-            <Navbar state={state.sideBar}/>
-            <div className={'app-wrapper-content'}>
-                <Route path={`/profile`}
-                       render={() => <Profile state={state.profilePage}
-                                              dispatch={dispatch}/>}/>
+                <Header/>
+                <Navbar state={state.sideBar}/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={`/profile`}
+                           render={() => <Profile state={state.profilePage}
+                                                  dispatch={dispatch}/>}/>
 
-                <Route exact={true} path={`/dialogs/:id?`}
-                       render={() => <Dialogs state={state.dialogsPage}
-                                              dispatch={dispatch}/>}/>
+                    <Route exact={true} path={`/dialogs/:id?`}
+                           render={() => <Dialogs state={state.dialogsPage}
+                                                  dispatch={dispatch}/>}/>
 
-                <Route path={`/news`} component={News}/>
-                <Route path={`/music`} component={Music}/>
-                <Route path={`/settings`} component={Settings}/>
+                    <Route path={`/news`} component={News}/>
+                    <Route path={`/music`} component={Music}/>
+                    <Route path={`/settings`} component={Settings}/>
+                </div>
             </div>
-        </div>
     );
-}
+};
 
 export default App;
 
