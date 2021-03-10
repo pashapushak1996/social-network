787ac57655e6ba0dc26d07adabd09a174cac2efb
pashapushak1996 <pashapushak1996@gmail.com>
2021-03-10 23:37:21 +0200

    l55(pagination)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import User from './User/User';
 import axios from 'axios';
+import styles from './Users.module.css';
 
-const Users = ({users, follow, unfollow, setUsers}) => {
-    const setAllUsers = () => {
-        if (users.length === 0) {
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(res => {
-                setUsers(res.data.items);
-            });
-        }
+class Users extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(res => {
+            debugger;
+            this.props.setTotalCount(res.data.totalCount);
+            this.props.setUsers(res.data.items);
+        });
     };
 
-    return (
+
+    followUser(id) {
+        this.props.follow(id);
+    };
+
+    unfollowUser = (id) => {
+        this.props.unfollow(id);
+    };
+
+    switchCurrentPage = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(res => {
+            this.props.setUsers(res.data.items);
+        });
+    };
+
+    render() {
+
+        const pageCount = Math.ceil(this.props.totalCount / this.props.pageSize);
+        const pages = [];
+
+        for (let i = 1; i <= pageCount; i++) {
+            pages.push(i);
+        }
+
+        const {users} = this.props;
+
+        return (<div>
             <div>
-                <button onClick={setAllUsers}>Set users</button>
-                {users.map((user) => <User key={user.id} user={user} follow={follow} unfollow={unfollow}/>)}
+                {pages.map((p) => {
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                                 onClick={() => this.switchCurrentPage(p)}> {p}</span>;
+                })}
             </div>
-    );
-};
+            {users.map((user) => <User key={user.id} user={user} follow={this.followUser.bind(this)}
+                                       unfollow={this.unfollowUser}/>)}
+        </div>);
+    }
+}
 
 
 export default Users;
